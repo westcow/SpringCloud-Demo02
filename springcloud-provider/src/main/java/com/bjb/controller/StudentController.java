4be7f2aa61f0088ab6b18c0319c921b7a4d32f63
westcow <1024668091@qq.com>
2021-03-29 00:24:43 +0800

    StudentController文件补充接口；添加启动类；application.yml修改了配置； 目前存在数据源配置问题（待解决）

--- springcloud-provider/src/main/java/com/bjb/controller/StudentController.java
+++ springcloud-provider/src/main/java/com/bjb/controller/StudentController.java
@@ -1,5 +1,10 @@
 package com.bjb.controller;
 
+import com.bjb.service.StudentService;
+import entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/queryStudentById/{sId}")
+    public Student queryStudentById(@PathVariable Integer sId) {
+        return studentService.queryStudentById(sId);
+    }
+
 }
---
+++ springcloud-provider/src/main/java/com/bjb/studentProvider_8001.java
@@ -0,0 +1,11 @@
+package com.bjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class studentProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(studentProvider_8001.class, args);
+    }
+}
